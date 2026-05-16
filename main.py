f4a52e3f637a897d98052997f9fe2c7c89acf038
seiffayed <seif.120230091@ejust.edu.eg>
2026-05-16 23:43:14 +0300

    Add Search Button Feature
    - Update the UI with the new button
    - Add new function for search for a saved password
    - Add some exceptions to the save password function to handle errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint,shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -21,28 +22,64 @@ def generate_password():
     password_entry.insert(0,password)
     pyperclip.copy(password)
 
+# ---------------------------- Search For Password ------------------------------- #
+def find_password():
+    website = website_entry.get()
+
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Please provide a website to search for password")
+    
+    else:
+        try:
+            with open("Test_passwords.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="No Data File Found.")
+        else:
+            if website in data:
+                username = data[website]["username"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Username: {username} \nPassword: {password}")
+            else:
+                messagebox.showerror(title="Error", message=f"No details for the {website} exists.")
+                
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "username":username,
+            "password":password
+        }
+    }
 
-    if len(website) ==0 or len(username) ==0 or len(password) ==0 :
+    if len(website) == 0 or len(username) == 0 or len(password) == 0 :
         messagebox.showinfo(title="Oops", message="Please make sure you haven't "
                                                     "left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, 
-                                    message=f"These are the details entered: "
-                                    f"\nUsername: {username}" 
-                                    f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("Test_passwords.txt", "a") as file:
-                file.write(f" Website: {website} | Username: {username} | Password: {password}\n")
-                website_entry.delete(0,END)
-                username_entry.delete(0,END)
-                password_entry.delete(0,END)
-                username_entry.insert(0, "your_email@gmail.com") 
-                website_entry.focus()
+        try:
+            with open("Test_passwords.json", "r") as file:
+                #Read old data
+                data = json.load(file)
+
+        except FileNotFoundError:
+            with open("Test_passwords.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+        
+            with open("Test_passwords.json", "w") as file:
+                #Saving old data with new data
+                json.dump(data, file, indent=4)
+
+        finally:
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
+            website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -66,8 +103,8 @@ password_lable = Label(text="Password:")
 password_lable.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW", padx=(5,0),  ) 
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, sticky="EW", padx=(5,0),  ) 
 website_entry.focus() 
 
 username_entry = Entry(width=35)
@@ -84,6 +121,7 @@ generate_password_button.grid(row=3, column=2, sticky="EW", padx=(5,0), pady=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW", padx=(5,0), pady=2)
 
-
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1,column=2,sticky="EW", padx=(5,0), pady=2) 
 
 window.mainloop()
